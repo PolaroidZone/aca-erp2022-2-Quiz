81c2677cf3a7476b904b91d19ba01a0d2f902587
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-08-06 00:18:22 +0200

    Creating the right side nav

---
+++ src/components/CharacterSelector.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CharacterSelector = () => {
+  return (
+    <div className="character-con">
+        <div className="charecter">
+            <div className="char-indicator"></div>
+            <div className="char-main">
+                <img src="" alt="" />
+                <h1>Shuri</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CharacterSelector
