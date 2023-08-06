efc229c0f1ec81456a45dab9abb2b0acacb10ec2
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-08-07 01:32:00 +0200

    Adding navigation fucntionality

---
+++ src/components/Images.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import spiderMan from '../images/hulk.png'
+import hulk from '../images/hulk.png'
+import blackWidow from '../images/black widow.png'
+import ironMan from '../images/Iron man.png'
+
+export default {spiderMan, hulk, blackWidow}
